fcc3e7f8f8c0aace09350f9f751fca4beda1e4a4
David Flores <nonnumericdave@gmail.com>
2016-03-15 23:29:01 -0600

    + Introduce an additional return value which specifies the number of elements which were parsed.
    -- daf

--- XMLParsingSpeed/DAFXMLParser.swift
+++ XMLParsingSpeed/DAFXMLParser.swift
@@ -11,12 +11,13 @@ import Foundation
 protocol DAFXMLParser
 {
     /**
-        Parse XML document located at URL file path in a SAX-like manner, 
+        Parse XML document located at URL file path in a SAX-like manner,
         returning the time interval required to complete the parse.
     
         - Parameter urlFilePath: The URL file path for the XML document.
      
-        - Returns: The time interval required to complete the parse.
+        - Returns: The time interval required to complete the parse, along with
+            the number of elements in the XML document.
      */
-    func SAXParseXMLFileAtPath(urlFilePath : NSURL) -> NSTimeInterval
+    func SAXParseXMLFileAtPath(urlFilePath : NSURL) -> (NSTimeInterval, UInt64)
 }
