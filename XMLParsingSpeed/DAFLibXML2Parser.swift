dfc81c9514ca7f4160078b327e71b22c12448b2b
David Flores <nonnumericdave@gmail.com>
2016-03-15 23:33:40 -0600

    + Stub out parser wrappers for testing libxml2 and NSXMLParser.
    -- daf

---
+++ XMLParsingSpeed/DAFLibXML2Parser.swift
@@ -0,0 +1,17 @@
+//
+//  DAFLibXML2Parser.swift
+//  XMLParsingSpeed
+//
+//  Created by David Flores on 3/15/16.
+//  Copyright © 2016 David Flores. All rights reserved.
+//
+
+import Foundation
+
+class DAFLibXML2Parser : DAFXMLParser
+{
+    func SAXParseXMLFileAtPath(urlFilePath : NSURL) -> (NSTimeInterval, UInt64)
+    {
+        return (0, 0)
+    }
+}
