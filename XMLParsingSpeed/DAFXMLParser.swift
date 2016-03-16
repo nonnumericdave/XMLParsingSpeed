64c8d9a58de37814065387454b856ed5f692cbf7
David Flores <nonnumericdave@gmail.com>
2016-03-15 23:24:55 -0600

    + Introduce DAFXMLParser protocol.
    -- daf

---
+++ XMLParsingSpeed/DAFXMLParser.swift
@@ -0,0 +1,22 @@
+//
+//  DAFXMLParser.swift
+//  XMLParsingSpeed
+//
+//  Created by David Flores on 3/15/16.
+//  Copyright © 2016 David Flores. All rights reserved.
+//
+
+import Foundation
+
+protocol DAFXMLParser
+{
+    /**
+        Parse XML document located at URL file path in a SAX-like manner, 
+        returning the time interval required to complete the parse.
+    
+        - Parameter urlFilePath: The URL file path for the XML document.
+     
+        - Returns: The time interval required to complete the parse.
+     */
+    func SAXParseXMLFileAtPath(urlFilePath : NSURL) -> NSTimeInterval
+}
